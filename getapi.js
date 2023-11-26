907f46a4e5ca438ff7887a9dd4e15831544b41b9
Robertino <gladdenrobertino@gmail.com>
2023-11-27 01:25:52 +0700

    initial commit

--- getapi.js
+++ getapi.js
@@ -1,25 +1,26 @@
-const dataEndpoint = "https://backend-cps.vercel.app/api"; // Ubah sesuai dengan endpoint yang benar dari API provinsi sampai desa
+const dataEndpoint = "http://localhost:3000/api"; // Ubah sesuai dengan endpoint yang benar dari API provinsi sampai desa
 
-const keyword = document.querySelector(".keyword");
+const keywordInput = document.querySelector(".keyword");
 const btnSearch = document.querySelector(".btn-search");
 const container = document.getElementById("container");
 
 btnSearch.onclick = () => {
-  fetch(`${dataEndpoint}`) // Mengirim permintaan ke endpoint API provinsi sampai desa
+  const keyword = keywordInput.value;
+  fetch(`${dataEndpoint}/${keyword}`) // Mengirim permintaan ke endpoint API berdasarkan ID
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      let elements = "";
-
-      data.forEach((item) => {
-        elements += showElement(item);
-      });
-
-      container.innerHTML = elements;
+      container.innerHTML = showElement(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      container.innerHTML = "<p>Data not found.</p>";
     });
 };
 
 function showElement(data) {
+  if (!data) return "<p>Data not found.</p>";
+
   return `
     <div class="province">
       <h3>Provinsi: ${data.provinsi}</h3>
