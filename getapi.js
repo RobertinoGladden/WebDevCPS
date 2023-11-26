a29acf08e364dc32c38df85542b0bc1a7436d90e
Robertino <gladdenrobertino@gmail.com>
2023-11-27 00:38:29 +0700

    initial commit

--- getapi.js
+++ getapi.js
@@ -1,43 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.querySelector('.btn-search');
-  const keywordInput = document.querySelector('.keyword');
-  const searchResults = document.querySelector('.search-results');
+const dataEndpoint = "https://backend-cps.vercel.app/api"; // Ubah sesuai dengan endpoint yang benar dari API provinsi sampai desa
 
-  searchButton.addEventListener('click', async () => {
-    const id = parseInt(keywordInput.value.trim());
+const keyword = document.querySelector(".keyword");
+const btnSearch = document.querySelector(".btn-search");
+const container = document.getElementById("container");
 
-    if (!isNaN(id)) {
-      try {
-        const response = await fetch(`/api/data/${id}`); // Mengambil data dari endpoint '/api/data/:id'
-        if (!response.ok) {
-          throw new Error('Data not found');
-        }
+btnSearch.onclick = () => {
+  fetch(`${dataEndpoint}`) // Mengirim permintaan ke endpoint API provinsi sampai desa
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let elements = "";
 
-        const foundData = await response.json();
+      data.forEach((item) => {
+        elements += showElement(item);
+      });
 
-        searchResults.innerHTML = '';
+      container.innerHTML = elements;
+    });
+};
 
-        const listItemProvince = document.createElement('li');
-        listItemProvince.textContent = `Province: ${foundData.provinsi}`;
-        searchResults.appendChild(listItemProvince);
-
-        const listItemCity = document.createElement('li');
-        listItemCity.textContent = `City: ${foundData.kota}`;
-        searchResults.appendChild(listItemCity);
-
-        const listItemSubdistrict = document.createElement('li');
-        listItemSubdistrict.textContent = `Subdistrict: ${foundData.kecamatan}`;
-        searchResults.appendChild(listItemSubdistrict);
-
-        const listItemVillage = document.createElement('li');
-        listItemVillage.textContent = `Village: ${foundData.desa}`;
-        searchResults.appendChild(listItemVillage);
-      } catch (error) {
-        console.error('Error:', error.message);
-        searchResults.innerHTML = `<li>${error.message}</li>`;
-      }
-    } else {
-      searchResults.innerHTML = '<li>Please enter a valid ID</li>';
-    }
-  });
-});
+function showElement(data) {
+  return `
+    <div class="province">
+      <h3>Provinsi: ${data.provinsi}</h3>
+      <p>Kota: ${data.kota}</p>
+      <p>Kecamatan: ${data.kecamatan}</p>
+      <p>Desa: ${data.desa}</p>
+    </div>
+  `;
+}
