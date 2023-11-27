0db84e6c9aa0609d40d2aa54f24519f34ba99d71
Robertino <gladdenrobertino@gmail.com>
2023-11-27 09:59:33 +0700

    initial commit

--- getapi.js
+++ getapi.js
@@ -2,24 +2,20 @@ const dataEndpoint = "https://backend-a1mv0rjx6-robertinogladden.vercel.app/api/
 
 const btnSearch = document.querySelector(".btn-search");
 const container = document.getElementById("container");
-const inputId = document.querySelector("#input-id");
+const inputId = document.querySelector(".keyword");
 
-btnSearch.onclick = async () => {
-  try {
-    const id = inputId.value; // Mendapatkan ID dari input field
-    const response = await fetch(`${dataEndpoint}${id}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await response.json();
-    console.log(data); // Menampilkan data di konsol
-
-    container.innerHTML = showElement(data);
-  } catch (error) {
+btnSearch.onclick = () => {
+  const keyword = keywordaInput.value;
+  fetch(`${dataEndpoint}/./${keyword}`)
+    .then((res) => res.json())
+    .then((data) =>{
+      console.log(data);
+      container.innerHTML = showElement(data);
+    })
+  .catch((error) => {
     console.error("Error fetching data:", error);
     container.innerHTML = "<p>Data not found.</p>";
-  }
+  });
 };
 
 function showElement(data) {
